2345c15cc8f1afe33526d2cd8561126470396a10
Cynthia Z E MacLeod (C3D) <Cynthia.MacLeod@cloud3d.ltd>
2021-04-08 16:04:57 +0100

    Remove extraneous code

--- RadioButtonIssue/RadioButtonIssue/MainPageViewModel.cs
+++ RadioButtonIssue/RadioButtonIssue/MainPageViewModel.cs
@@ -8,17 +8,11 @@ namespace RadioButtonIssue
 {
     public class MainPageViewModel : ObservableObject
     {
-        private bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
-        {
-            return base.SetProperty(ref field, newValue, propertyName);
-        }
-
         private UInt32 channel = 1;
 
         public MainPageViewModel()
         {
             Device.StartTimer(TimeSpan.FromSeconds(3), ChangeChannel);
-            //var comp = StringComparer.InvariantCultureIgnoreCase;
         }
 
         private bool ChangeChannel()
@@ -37,18 +31,10 @@ namespace RadioButtonIssue
         }
 
         public UInt32 Channel { get => channel; set => SetProperty(ref channel, value, onChanged: ChannelChanged); }
-        public object ChannelO { get => $"Ch{Channel}"; set => Channel = uint.Parse(((string)value).Substring(2)); }
 
         private async void ChannelChanged()
         {
-            OnPropertyChanged(nameof(ChannelO));
             await Application.Current.MainPage.DisplayToastAsync($"Channel {Channel}").ConfigureAwait(false);
         }
-
-        private string enteredCode;
-
-        public string EnteredCode { get => enteredCode; set => SetProperty(ref enteredCode, value); }
-
-
     }
 }
